b179ae6b0ca05ceddfccfeb330c8075f0a5ca254
Divyath ali shajahan <divyathalishajahan@gmail.com>
2023-03-10 11:52:32 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+export default{
+    JWT_SECRET:"TXNQzh&dckQ/Kb7wXhw2IaYI/cHd7y7VRezaLHeW0Qs=",
+    EMAIL:"austyn.krajcik56@ethereal.email",
+    PASSWORD:"ggs5VCSCRt4JRBdmVZ",
+    ATLAS_URI:"mongodb+srv://admin:admin123@cluster0.gpoflfg.mongodb.net/?retryWrites=true&w=majority"
+}
